64bbf0c0ed82f89e3c8ff2bff9a9162075a20341
Xet Erixon <xet.erixon@gmail.com>
2015-01-26 19:36:23 +0100

    Tests
    Gallery view

---
+++ GalleryTest/GalleryTest.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+using Xamarin.Forms;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace GalleryTest
+{
+	class ImageData{
+		public string Caption{get;set;}
+		public string Image{get;set;}
+	}
+	public class App : Application
+	{
+
+		ObservableCollection<ImageData> ListOfStuff;
+		List<string> ImageUrls{ get; set; }
+		public App ()
+		{
+			// Some random images to use...
+			ImageUrls = new List<string>
+			{
+				"http://cdn.filipekberg.se/fekberg-blog/wp-content/uploads/2014/04/VS2013Logo.png",
+				"https://dynamicimagesen-v2b.netdna-ssl.com/product/en_97_professional-c-sharp-vol2.jpg",
+				"http://taswar.zeytinsoft.com/wp-content/uploads/2012/11/dotnet.png",
+				"http://charlespetzold.com/pwcs/pwcs.png"
+
+			};
+			// this is the list of images
+			ListOfStuff = new ObservableCollection<ImageData> ();
+			MainPage = new ContentPage {
+				Content = new ScrollView
+				{
+					Content = new GalleryView {
+						HorizontalOptions = LayoutOptions.FillAndExpand,
+						VerticalOptions = LayoutOptions.FillAndExpand,
+						Orientation = StackOrientation.Horizontal,
+						ItemsSource = ListOfStuff
+					}
+				}
+			};
+			int index = 0;
+			Device.StartTimer (TimeSpan.FromSeconds (2), () => {
+				ListOfStuff.Add(
+					new ImageData{
+						Caption = DateTime.Now.ToString(),
+						Image = ImageUrls[index] 
+					}
+				);
+				index++;
+				return ImageUrls.Count > index ;
+			});
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
+
---
+++ GalleryTest/GalleryView/GalleryView.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Xamarin.Forms;
+using System.Collections;
+using System.ComponentModel;
+using System.Collections.Specialized;
+
+namespace GalleryTest
+{
+	public class GalleryViewCell : ContentView
+	{
+		private Label _caption;
+		private Image _image;
+		public GalleryViewCell()
+		{
+			HeightRequest = 100;
+			WidthRequest = 100;
+			BackgroundColor = Color.Gray;
+			_caption = new Label{ HeightRequest = 18, WidthRequest = 100 };
+			_image = new Image{ WidthRequest = 100, HeightRequest = 70, BackgroundColor = Color.Yellow };
+			_image.SetBinding (Image.SourceProperty, "Image");
+			_caption.SetBinding(Label.TextProperty,  "Caption");
+			StackLayout layout = new StackLayout
+			{ 
+				Orientation = StackOrientation.Vertical,
+				Children = {
+					_image,
+					_caption
+				}
+			};
+			Content = layout;
+		}
+
+	}
+	/// <summary>
+	/// Gallery view.
+	/// The ItemsSource property should be bound to a ObservableCollection.
+	/// </summary>
+	public class GalleryView : WrapLayout
+	{
+		public static readonly BindableProperty ItemsSourceProperty = BindableProperty.Create<GalleryView,IEnumerable> ((prop) => prop.ItemsSource, default(IEnumerable), 
+			propertyChanged:(bindable, oldValue, newValue) =>  ((GalleryView)bindable).ImageSourceChanged(),
+			propertyChanging:(bindable, oldValue, newValue) =>  ((GalleryView)bindable).ImageSourceAboutToChange()
+		);
+
+
+		public IEnumerable ItemsSource {
+			get{ return (IEnumerable)GetValue (ItemsSourceProperty); }
+			set{ SetValue (ItemsSourceProperty, value); }
+		}
+		private void ImageSourceChanged()
+		{
+			var occ = this.ItemsSource as INotifyCollectionChanged;
+			if (occ != null) {
+				occ.CollectionChanged += HandleCollectionChanged;
+				AddItems (ItemsSource);
+			}
+
+		}
+		private void ImageSourceAboutToChange()
+		{
+			var occ = ItemsSource as INotifyCollectionChanged;
+			if (occ != null) {
+				occ.CollectionChanged -= HandleCollectionChanged;
+				Children.Clear ();
+			}
+		}
+		private void AddItems(IEnumerable list)
+		{
+			foreach (var item in list)
+			{
+				Children.Add ( new GalleryViewCell{ BindingContext = item});
+			}
+
+		}
+
+		protected void HandleCollectionChanged (object sender, NotifyCollectionChangedEventArgs e)
+		{
+			// Only dealing with Reset and Add actions for now.
+			// Implement the others yourself ... ;)
+			if (e.Action == NotifyCollectionChangedAction.Reset) {
+				Children.Clear ();
+			}
+			else if (e.Action == NotifyCollectionChangedAction.Add) 
+			{
+				if (e.NewItems != null) 
+				{
+					AddItems (e.NewItems);
+				}
+			}
+			
+		}
+		public GalleryView ()
+		{
+		}
+	}
+}
+
